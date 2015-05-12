cc3abc3fa767bed0000a38ecea0d65b5e8edb59f
Ilia <ilia.reingold@gmail.com>
2015-05-11 20:43:53 -0400

    fresh start

--- both/router.js
+++ both/router.js
@@ -1,9 +1,9 @@
 Router.onBeforeAction(function() {
-  if (!Meteor.user()) {
-    this.render('authenticate');
-  } else {
-    this.next();
-  }
+  // if (!Meteor.user()) {
+  //   this.render('authenticate');
+  // } else {
+     this.next();
+  // }
 });
 
 Router.configure({
@@ -18,25 +18,4 @@ Router.route('/', {
 Router.route('/authenticate', {
   template: "authenticate",
   name: "authenticate"
-});
-
-Router.route('search');
-Router.route('new');
-
-Router.route('/show/:id', {
-  // this template will be rendered until the subscriptions are ready
-  loadingTemplate: 'loading',
-
-  waitOn: function () {
-    // return one handle, a function, or an array
-    return Meteor.subscribe('decision', this.params.id);
-  },
-
-  data: function() {
-    return Decisions.findOne(this.params.id);
-  },
-
-  action: function () {
-    this.render('show');
-  }  
 });
