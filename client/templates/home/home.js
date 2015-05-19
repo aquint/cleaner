6e7e1283ecac38f150d200edc67c59b98c23c799
Ilia <ilia.reingold@gmail.com>
2015-05-19 19:54:29 -0400

    smashing the old js code into a controller

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -14,7 +14,7 @@ Template.landing.events({
       Session.setPersistent("postalCode", postalCode);
     }
 
-    Router.go("/tabs/one");
+    Router.go("/vendor");
     
       
     // else {
--- client/templates/tabs/2/ddp/ddp.js
+++ client/templates/tabs/2/ddp/ddp.js
@@ -3,91 +3,30 @@
 var remote = DDP.connect("http://localhost:3000/");
 Vendors = new Mongo.Collection('vendors', remote);
 
-Template.tabsTwo.onCreated(function(){
-  remote.subscribe('vendors', function(){
-    //var vendors = Vendors.find({});
-  })
-});
-
-Template.tabsTwo.helpers({
+Template.tabsLayout.helpers({
   vendor: function() {
     return Vendors.find({});
   }
 })
 
-Router.map(function() {
-  this.route('tabs.one', {path: '/tabs/one', layoutTemplate: 'tabsLayout'});
-  this.route('tabs.two', {path: '/tabs/two', layoutTemplate: 'tabsLayout'});
-  this.route('tabs.three', {path: '/tabs/three', layoutTemplate: 'tabsLayout'});
+// tabs controller
 
-  // Subtabs in cleaner detail page
-  this.route('tabs.two.cleaner-detail.about/:id', {
-    path: '/tabs/two/cleaner-detail/about/:id', 
-    layoutTemplate: 'tabsLayout', 
-    template: "tabsTwoCleanerDetail",
-    waitOn: function () {
-      // return one handle, a function, or an array
-      return remote.subscribe('vendors', this.params.id); 
-    },
-    data: function() {
-      return Vendors.findOne(this.params.id);
-    },
-    yieldTemplates: {
-      'tabsTwoCleanerDetailAbout': {to: "subtabs"}
-    }
-  });
-  this.route('tabs.two.cleaner-detail.pricing/:id', {
-    path: '/tabs/two/cleaner-detail/pricing/:id', 
-    layoutTemplate: 'tabsLayout', 
-    template: "tabsTwoCleanerDetail",
-    waitOn: function () {
-      // return one handle, a function, or an array
-      return remote.subscribe('vendors', this.params.id); 
-    },
-    data: function() {
-      return Vendors.findOne(this.params.id);
-    },
-    yieldTemplates: {
-      'tabsTwoCleanerDetailPricing': {to: "subtabs"}
-    }
-  });
-  this.route('tabs.two.cleaner-detail.book/:id', {
-    path: '/tabs/two/cleaner-detail/book/:id', 
-    layoutTemplate: 'tabsLayout', 
-    template: "tabsTwoCleanerDetail",
-    waitOn: function () {
-      // return one handle, a function, or an array
-      return remote.subscribe('vendors', this.params.id); 
-    },
-    data: function() {
-      return Vendors.findOne(this.params.id);
-    },
-    yieldTemplates: {
-      'tabsTwoCleanerDetailBook': {to: "subtabs"}
-    }
-  });
-  this.route('tabs.two.cleaner-detail.reviews/:id', {
-    path: '/tabs/two/cleaner-detail/reviews/:id', 
-    layoutTemplate: 'tabsLayout', 
-    template: "tabsTwoCleanerDetail",
-    waitOn: function () {
-      // return one handle, a function, or an array
-      return remote.subscribe('vendors', this.params.id); 
-    },
-    data: function() {
-      return Vendors.findOne(this.params.id);
-    },
-    yieldTemplates: {
-      'tabsTwoCleanerDetailReviews': {to: "subtabs"}
-    }
-  });
+VendorController = RouteController.extend({
+  layoutTemplate: 'tabsLayout',
+  template: "tabsTwoCleanerDetail",
+  waitOn: function() { return remote.subscribe('vendors'); },
+  data: function() { return Vendors.find({}); },
+  action: function() {
+    this.render('tabsTwo');
+    //this.render('tabsTwoCleanerDetailAbout', { to: 'subtabs' });
+  }
 });
 
+// routes
 
-Template._subTabs.helpers({
-  id: function() {
-    var url = location.href;
-    var array = url.split('/');
-    return array[array.length-1];
-  }
-})
+Router.route('/vendors', {
+  name: 'vendors',
+  controller: 'VendorController',
+  where: 'client',
+  action: 'action'
+});
