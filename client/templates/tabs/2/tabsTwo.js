43d22e03435f910ab2f33780fa63d14c8eacbfdc
Ilia <ilia.reingold@gmail.com>
2015-05-19 21:54:33 -0400

    string wont do add class for some weird reason, shall tackle later

--- client/templates/tabs/2/tabsTwo.js
+++ client/templates/tabs/2/tabsTwo.js
@@ -1,38 +1,20 @@
-Template.tabsTwoCleanerDetail.onRendered(function(){
-  $('.ui.rating').rating('disable');
-  $('.datepicker').pickadate();
-
-  // highlight ABOUT
-  
-  $('.subtab-about').addClass('active');
-
-  $('.tabs-cleaner').addClass('active');
-  
-});
-
-Template.tabsTwoCleanerDetail.onDestroyed(function(){
-  $('.subtab-about').removeClass('active');
-  $('.tabs-cleaner').removeClass('active');
-})
-
-// ---------------------------------------
-
 Template.tabsTwo.rendered = function () {
-  $('.tabs-cleaner').addClass('active');
   $('.ui.rating').rating('disable');
 };
 
-Template.tabsTwo.onDestroyed(function () {
-  $('.tabs-cleaner').removeClass('active');
+Template.tabsTwoCleanerDetailAbout.onRendered(function(){
+  activate('about');
 });
 
-Template.tabsOne.rendered = function() {
-  $('.tabs-clean').addClass('active')
-};
+function activate(value) {
+  $('.tabs-cleaner').addClass('active');
 
-Template.tabsOne.onDestroyed(function () {
-  $('.tabs-clean').removeClass('active')
-});
+  var string = '.tabs-' + value; 
+  console.log(string);
+  check(string, String); 
+  $(string).addClass('active');
+}
+
 
 
 
